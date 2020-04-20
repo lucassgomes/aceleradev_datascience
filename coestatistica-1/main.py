89ec81d23eeda9859566b7cd28583b058223ea94
Lucas Soares Gomes <lucas.s.gomes@ufv.br>
2020-04-20 19:36:12 -0300

    Week 2 and 3 - data pre-processing and exploratory analysis in python

---
+++ coestatistica-1/main.py
@@ -0,0 +1,22 @@
+import pandas as pd
+
+
+def main():
+    df = pd.read_csv('desafio1.csv')
+    pts_credit = df.groupby('estado_residencia').pontuacao_credito.agg([
+        pd.Series.mode,
+        'median',
+        'mean',
+        'std'
+    ])
+    pts_credit.columns = [
+        'moda',
+        'mediana',
+        'media',
+        'desvio_padrao'
+    ]
+    pts_credit.to_json('submission.json', orient='index')
+
+
+if __name__ == '__main__':
+    main()
